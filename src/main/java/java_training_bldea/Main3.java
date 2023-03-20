8e830f354f3f61796e2dcf8a6500dbde05b1d169
Admin <Admin@DESKTOP-IBP9R3R>
2023-03-20 13:20:29 +0530

    Commit2

--- src/main/java/java_training_bldea/Main3.java
+++ src/main/java/java_training_bldea/Main3.java
@@ -1,5 +1,19 @@
-package java_training_bldea;
-
-public class Main3 {
-
+import java.util.Scanner;
+public class Code4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n : ");
+        int n = sc.nextInt();
+        for (int i=0;i<n;i++){
+            for (int j=0;j<n;j++){
+                if (i==0 || i==(n-1) || j==0 || j==(n-1) || j==(n/2) || i==(n/2)                       || i==j || i+j==(n-1) || i+j==(n/2) || j-i==(n/2) || i-j==                            (n/2) || i+j==(n-1)+(n/2) ) {
+                System.out.print("* ");
+            }
+            else {
+                System.out.print("  ");
+            }
+        }
+        System.out.println();  
+    }
+}
 }
